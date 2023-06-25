2b9e430436db99023b2d5eb3c5690aff7c6a2b8c
Catherine Tung <catt.tung@gmail.com>
2023-06-25 12:37:24 -0400

    copy files over from original taco cat

---
+++ routes/profiles.js
@@ -0,0 +1,17 @@
+import { Router } from 'express'
+import { isLoggedIn } from '../middleware/middleware.js'
+import * as profilesCtrl from "../controllers/profiles.js"
+
+const router = Router()
+
+// GET - localhost:3000/profiles
+router.get("/", isLoggedIn, profilesCtrl.index)
+// GET - localhost:3000/profiles/:id
+router.get("/:id", isLoggedIn, profilesCtrl.show)
+
+// POST - localhost:3000/profiles/:id/cats
+router.post("/:id/cats", isLoggedIn, profilesCtrl.createCat)
+
+export {
+  router
+}
